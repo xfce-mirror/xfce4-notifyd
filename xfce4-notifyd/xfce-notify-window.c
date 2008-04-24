bbbcd61e3fffa7d0dfc2e6f8d3ee0620088da835
Brian J. Tarricone <bjt23@cornell.edu>
2008-04-23 23:22:07 -0700

    set some extra window hints

--- xfce4-notifyd/xfce-notify-window.c
+++ xfce4-notifyd/xfce-notify-window.c
@@ -207,6 +207,9 @@ xfce_notify_window_init(XfceNotifyWindow *window)
     gtk_window_set_accept_focus(GTK_WINDOW(window), FALSE);
     gtk_window_set_skip_taskbar_hint(GTK_WINDOW(window), TRUE);
     gtk_window_set_skip_pager_hint(GTK_WINDOW(window), TRUE);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+    gtk_window_set_type_hint(GTK_WINDOW(window),
+                             GDK_WINDOW_TYPE_HINT_NOTIFICATION);
     gtk_container_set_border_width(GTK_CONTAINER(window), BORDER * 2);
     gtk_widget_set_app_paintable(GTK_WIDGET(window), TRUE);
     gtk_widget_add_events(GTK_WIDGET(window),
