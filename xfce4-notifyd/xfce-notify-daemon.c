c5ec8e7d04c458b382df6b02271763c56471fc54
Olivier Duchateau <duchateau.olivierd@gmail.com>
2016-09-06 19:44:59 +0000

    Fix client pointer with Gtk+ < 3.20 (Bug #12823)
    Signed-off-by: Simon Steinbeiss <simon.steinbeiss@elfenbeinturm.at>

--- xfce4-notifyd/xfce-notify-daemon.c
+++ xfce4-notifyd/xfce-notify-daemon.c
@@ -706,6 +706,7 @@ xfce_notify_daemon_window_size_allocate(GtkWidget *widget,
     widget_screen = gtk_widget_get_screen (widget);
     display = gdk_screen_get_display (widget_screen);
     device_manager = gdk_display_get_device_manager (display);
+    pointer = gdk_device_manager_get_client_pointer (device_manager);
 #endif
 
     gdk_device_get_position (pointer, &p_screen, &x, &y);
