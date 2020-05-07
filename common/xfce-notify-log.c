d804080984907b4277008010fbbfbcbedfa9b48f
Jan Ziak <0xe2.0x9a.0x9b@gmail.com>
2020-05-07 15:15:40 +0200

    Fix memory leaks
    Signed-off-by: Jan Ziak <0xe2.0x9a.0x9b@gmail.com>

--- common/xfce-notify-log.c
+++ common/xfce-notify-log.c
@@ -266,6 +266,7 @@ void xfce_notify_log_insert (const gchar *app_name,
         g_warning ("Unable to open cache file");
 
     g_free (notify_log_path);
+    g_free (notify_log_icon_folder);
 }
 
 gchar *
