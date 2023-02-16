7d0d99c1c797ab77241638aba9d60b6c97eeee4f
Brian J. Tarricone <brian@tarricone.org>
2023-02-15 20:03:47 -0800

    Make all notification log access go through dbus
    This should fix issues where unread state wasn't getting noticed
    properly by the plugin or other entities, and makes log change
    notification granular and... well... just better.
    
    Also this allows the daemon to "own" the log and be the only process
    that reads from and writes to it.  This way, the plugin and config
    dialog can't ever break the DB through version incompatibilities, and
    failure modes will be better-handled.
    
    Closes #90.

---
+++ common/xfce-notify-log-types.h
@@ -0,0 +1,54 @@
+/*
+ *  xfce4-notifyd
+ *
+ *  Copyright (c) 2023 Brian Tarricone <brian@tarricone.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License ONLY.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef __XFCE_NOTIFY_LOG_TYPES_H__
+#define __XFCE_NOTIFY_LOG_TYPES_H__
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef struct _XfceNotifyLogEntryAction {
+    gchar *id;
+    gchar *label;
+} XfceNotifyLogEntryAction;
+
+typedef struct _XfceNotifyLogEntry {
+    gchar *id;
+    GDateTime *timestamp;
+    gchar *app_id;
+    gchar *app_name;
+    gchar *icon_id;
+    gchar *summary;
+    gchar *body;
+    GList *actions;
+    gint expire_timeout;
+    gboolean is_read;
+
+    /*< private >*/
+    gatomicrefcount ref_count;
+} XfceNotifyLogEntry;
+
+XfceNotifyLogEntry *xfce_notify_log_entry_new_empty(void);
+XfceNotifyLogEntry *xfce_notify_log_entry_ref(XfceNotifyLogEntry *entry);
+void xfce_notify_log_entry_unref(XfceNotifyLogEntry *entry);
+
+G_END_DECLS
+
+#endif  /* __XFCE_NOTIFY_LOG_TYPES_H__ */
