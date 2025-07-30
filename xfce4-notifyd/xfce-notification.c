xfce-mirror/xfce4-notifyd

commit 630102f2d6c864c7d5e42804b7f5c75f0f552f44

Remove notification window from list when destroyed
It seems that the notification window can sometimes be unintentionally
destroyed, in which case it should be removed from the list. This
happens for example on wayland with labwc >= 0.9.0 when changing tty
(without fallback output configured in labwc), which causes first a
critical warning and eventually a crash.

xfce4-notifyd/xfce-notification.c
@@ -673,6 +673,13 @@ xfce_notification_window_action_invoked(XfceNotifyWindow *window,
 }
 
 static void
+xfce_notification_window_destroyed(XfceNotifyWindow *window,
+                                   XfceNotification *notification)
+{
+    notification->windows = g_list_remove(notification->windows, window);
+}
+
+static void
 xfce_notification_window_closed(XfceNotifyWindow *window,
                                 XfceNotifyCloseReason reason,
                                 XfceNotification *notification)
@@ -681,6 +688,7 @@ xfce_notification_window_closed(XfceNotifyWindow *window,
 
     g_signal_emit(notification, signals[SIG_CLOSED], 0, reason);
 
+    g_signal_handlers_disconnect_by_func(window, xfce_notification_window_destroyed, notification);
     g_list_free_full(notification->windows, (GDestroyNotify)gtk_widget_destroy);
     notification->windows = NULL;
 
@@ -818,6 +826,8 @@ create_notify_window(XfceNotification *notification,
                      G_CALLBACK(xfce_notification_window_action_invoked), notification);
     g_signal_connect(window, "closed",
                      G_CALLBACK(xfce_notification_window_closed), notification);
+    g_signal_connect(window, "destroy",
+                     G_CALLBACK(xfce_notification_window_destroyed), notification);
 
     return window;
 }

commit e1551ac5fdda6ef7f3cae1fdd6b7d282f4fc6402

Properly disconnect signal handlers when freeing window list
I've only done half the work in 630102f2, which may cause a double-free
of the list items.

Fixes: 630102f2d6c864c7d5e42804b7f5c75f0f552f44

xfce4-notifyd/xfce-notification.c
@@ -298,6 +298,23 @@ xfce_notification_constructed(GObject *object) {
 }
 
 static void
+xfce_notification_window_destroyed(XfceNotifyWindow *window,
+                                   XfceNotification *notification)
+{
+    notification->windows = g_list_remove(notification->windows, window);
+}
+
+static void
+xfce_notification_free_window_list(XfceNotification *notification) {
+    for (GList *lp = notification->windows; lp != NULL; lp = lp->next) {
+        g_signal_handlers_disconnect_by_func(lp->data, xfce_notification_window_destroyed, notification);
+        gtk_widget_destroy(lp->data);
+    }
+    g_list_free(notification->windows);
+    notification->windows = NULL;
+}
+
+static void
 xfce_notification_finalize(GObject *object) {
     XfceNotification *notification = XFCE_NOTIFICATION(object);
 
@@ -320,7 +337,7 @@ xfce_notification_finalize(GObject *object) {
 #endif
 
     // FIXME: probably not what we want?
-    g_list_free_full(notification->windows, (GDestroyNotify)gtk_widget_destroy);
+    xfce_notification_free_window_list(notification);
 
     G_OBJECT_CLASS(xfce_notification_parent_class)->finalize(object);
 }
@@ -673,13 +690,6 @@ xfce_notification_window_action_invoked(XfceNotifyWindow *window,
 }
 
 static void
-xfce_notification_window_destroyed(XfceNotifyWindow *window,
-                                   XfceNotification *notification)
-{
-    notification->windows = g_list_remove(notification->windows, window);
-}
-
-static void
 xfce_notification_window_closed(XfceNotifyWindow *window,
                                 XfceNotifyCloseReason reason,
                                 XfceNotification *notification)
@@ -688,9 +698,7 @@ xfce_notification_window_closed(XfceNotifyWindow *window,
 
     g_signal_emit(notification, signals[SIG_CLOSED], 0, reason);
 
-    g_signal_handlers_disconnect_by_func(window, xfce_notification_window_destroyed, notification);
-    g_list_free_full(notification->windows, (GDestroyNotify)gtk_widget_destroy);
-    notification->windows = NULL;
+    xfce_notification_free_window_list(notification);
 
     g_object_unref(notification);
 }